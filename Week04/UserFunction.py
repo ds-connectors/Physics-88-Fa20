09b3f146ef192246d002cea0e281aab14349fe2c
Yury <ygkolomensky@lbl.gov>
2020-09-21 13:42:50 -0700

    Lecture for Week3

---
+++ Week04/UserFunction.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+def Gauss(x, mu=0, sigma=1):
+    return 1./np.sqrt(2*np.pi)/sigma*np.exp(-0.5*(x-mu)**2/sigma**2)
---
+++ Week04/roundoff.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+  float x = 3/2;
+  printf("x = %f\n",x);
+
+  float y = 3./2;
+  printf("y = %f\n",y);
+
+  return 0;
+}
+
